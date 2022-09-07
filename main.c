b9f1d61f21a02809f970d11c13bcf56d5a0b2b6f
EdoardoCisco <ciscoedoardo@gmail.com>
2022-09-07 19:05:49 +0200

    all micro works

--- main.c
+++ main.c
@@ -53,14 +53,19 @@ int16_t data_output[SAMPLE_LENGTH];
 
 volatile int switch_data = 0;
 
-static uint_fast16_t printedBPM=100;
-uint_fast16_t BPM_val = BPM_COUNTER(100);
+/* flag moving*/
 uint8_t selectedOptionFlag=0;
 uint8_t OldFlag;
+
+/*screen*/
 Graphics_Context g_sContext;
-/* ADC results buffer */
+
+/* ADC results buffer I/O joystick*/
 static uint16_t resultBuffer[2];
-const uint_fast16_t* ptr = (const)&BPM_val ;
+
+/*metronome control*/
+static uint_fast16_t printedBPM=100;
+uint_fast16_t BPM_val = BPM_COUNTER(100);
 
 /* Timer_A PWM Configuration Parameter */
 const Timer_A_PWMConfig pwmConfig =
@@ -78,7 +83,6 @@ Timer_A_UpModeConfig upMetronome =
 {
     TIMER_A_CLOCKSOURCE_ACLK,//set
     TIMER_A_CLOCKSOURCE_DIVIDER_1,//set
-    //BPM_COUNTER(printedBPM),//set
     0,
     TIMER_A_TAIE_INTERRUPT_ENABLE,
     TIMER_A_CCIE_CCR0_INTERRUPT_DISABLE,
@@ -102,6 +106,7 @@ const Timer_A_UpModeConfig upBuzzer = {
         TIMER_A_DO_CLEAR                       
         };
 
+/* rectangle for Tuner*/
 const Graphics_Rectangle cleanMet = {38, 88, 84, 96};
 const Graphics_Rectangle barRedLeft = { 5, 82, 15, 94};
 const Graphics_Rectangle barRedRight = {113, 82, 123, 94};
@@ -110,30 +115,34 @@ const Graphics_Rectangle barYellowRight = {96, 72, 108, 104};
 const Graphics_Rectangle barGreenHalf = {56, 57, 72, 117};
 
 
-
+/*screen colors*/
 void cleanScreen();
-
 void printChoice();
 void selectOption();
 
+/*metronome control*/
 void incrementBPMvalue();
 void decrementBPMvalue();
 char* printChar();
 void metronomeBPMValue();
 void selectedDigit();
+void buzz();
+
+/* Tuner control*/
 char noteDetection();//rw
 int noteCorrection(char);//rw
-void buzz();
-uint_fast8_t prova = 0;
+void drawNote();//ne
+void drawBar();//rw
+void hannConfig();
 
+/* Handlers */
+void DMA_INT1_IRQHandler(void);
 void ADC14_IRQHandler(void);
 void PORT3_IRQHandler(void);
 void PORT4_IRQHandler(void);
 void PORT5_IRQHandler(void);
 void TA1_N_IRQHanlder(void);
 
-void drawNote();//ne
-void drawBar(uint8_t *);//rw
 
 
 void _adcInit() {
@@ -220,12 +229,74 @@ void _buzzerInit(){
 
     /* Initialize compare registers to generate PWM */
     Timer_A_initCompare(TIMER_A0_BASE, &compareConfigBuzzer); // For P2.7
-/*setup timer for metronome*/
+
+    /*setup timer for metronome*/
+    Timer_A_configureUpMode(TIMER_A1_BASE, &upMetronome);
+    Interrupt_enableInterrupt(INT_TA1_N);
+    Interrupt_enableMaster();
 }
 
+void _graphicsInit(){
+    /* Initializes display */                                            
+        Crystalfontz128x128_Init();                                    
+                                                                          
+        /* Set default screen orientation */                              
+        Crystalfontz128x128_SetOrientation(LCD_ORIENTATION_UP);
 
-int main(void)
+        /* Initializes graphics context */
+      Graphics_initContext(&g_sContext, &g_sCrystalfontz128x128,
+                           &g_sCrystalfontz128x128_funcs);
+      Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_BLACK);
+      Graphics_setBackgroundColor(&g_sContext, GRAPHICS_COLOR_WHITE);
+      GrContextFontSet(&g_sContext, &g_sFontFixed6x8);
+      Graphics_clearDisplay(&g_sContext);
+
+}
+
+void _dmaInit()
 {
+   Timer_A_generatePWM(TIMER_A2_BASE, &pwmConfig);
+    /* Initializing ADC (MCLK/1/1) */
+ ADC14_enableModule();
+ ADC14_initModule(ADC_CLOCKSOURCE_MCLK, ADC_PREDIVIDER_1, ADC_DIVIDER_1, 0);
+ ADC14_setSampleHoldTrigger(ADC_TRIGGER_SOURCE5, false);
+  /* Configuring GPIOs (4.3 A10) */
+ GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P4, GPIO_PIN3, GPIO_TERTIARY_MODULE_FUNCTION);
+  /* Configuring ADC Memory */
+ ADC14_configureSingleSampleMode(ADC_MEM2, true);
+ ADC14_configureConversionMemory(ADC_MEM2, ADC_VREFPOS_AVCC_VREFNEG_VSS, ADC_INPUT_A10, false);
+  /* Set ADC result format to signed binary */
+ ADC14_setResultFormat(ADC_SIGNED_BINARY);
+ /* Configuring DMA module */
+ DMA_enableModule();
+ DMA_setControlBase(MSP_EXP432P401RLP_DMAControlTable);
+ DMA_disableChannelAttribute( DMA_CH7_ADC14, UDMA_ATTR_ALTSELECT | UDMA_ATTR_USEBURST | UDMA_ATTR_HIGH_PRIORITY | UDMA_ATTR_REQMASK );
+ /* Setting Control Indexes. In this case we will set the source of the
+  * DMA transfer to ADC14 Memory 0
+  *  and the destination to the
+  * destination data array. */
+ DMA_setChannelControl( UDMA_PRI_SELECT | DMA_CH7_ADC14, UDMA_SIZE_16 | UDMA_SRC_INC_NONE | UDMA_DST_INC_16 | UDMA_ARB_1);
+ DMA_setChannelTransfer( UDMA_PRI_SELECT | DMA_CH7_ADC14, UDMA_MODE_PINGPONG, (void*) &ADC14->MEM[2], data_array1, SAMPLE_LENGTH);
+
+ DMA_setChannelControl( UDMA_ALT_SELECT | DMA_CH7_ADC14, UDMA_SIZE_16 | UDMA_SRC_INC_NONE | UDMA_DST_INC_16 | UDMA_ARB_1);
+ DMA_setChannelTransfer(UDMA_ALT_SELECT | DMA_CH7_ADC14, UDMA_MODE_PINGPONG, (void*) &ADC14->MEM[2], data_array2, SAMPLE_LENGTH);
+
+ /* Assigning/Enabling Interrupts */
+ DMA_assignInterrupt(DMA_INT1, 7);
+ Interrupt_enableInterrupt(INT_DMA_INT1);
+ DMA_assignChannel(DMA_CH7_ADC14);
+ DMA_clearInterruptFlag(7);
+ Interrupt_enableMaster();
+ /* Now that the DMA is primed and setup, enabling the channels. The ADC14
+  * hardware should take over and transfer/receive all bytes */
+ DMA_enableChannel(7);
+ ADC14_enableConversion();
+
+ hannConfig();
+
+}
+
+void _hwInit(){
     /* Halting WDT and disabling master interrupts */
     WDT_A_holdTimer();
     Interrupt_disableMaster();
@@ -243,122 +314,24 @@ int main(void)
     CS_initClockSignal(CS_HSMCLK, CS_REFOCLK_SELECT, CS_CLOCK_DIVIDER_128); // joystick
     CS_initClockSignal(CS_SMCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1); // pwm sampler, microphone 
     CS_initClockSignal(CS_ACLK, CS_REFOCLK_SELECT, CS_CLOCK_DIVIDER_1);
-    printf("prova2\n");
+
+    _graphicsInit();
     _adcInit();
     setHandlersGPIO();
     _buzzerInit();
-    printf("prova3\n");
-    
-    /* Initializes display */                                            
-        Crystalfontz128x128_Init();                                    
-                                                                          
-        /* Set default screen orientation */                              
-        Crystalfontz128x128_SetOrientation(LCD_ORIENTATION_UP);
+    _dmaInit();
+}
 
-        /* Initializes graphics context */
-      Graphics_initContext(&g_sContext, &g_sCrystalfontz128x128,
-                           &g_sCrystalfontz128x128_funcs);
-      Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_BLACK);
-      Graphics_setBackgroundColor(&g_sContext, GRAPHICS_COLOR_WHITE);
-      GrContextFontSet(&g_sContext, &g_sFontFixed6x8);
-      Graphics_clearDisplay(&g_sContext);
 
-        printf("BPM_val:%d\t%d\n ",BPM_val, &BPM_val);
-        printf("BPM_val: %d\t%d\t%d\n ",ptr, &ptr, *ptr);
+int main(void)
+{
+    _hwInit();
 
-        Timer_A_configureUpMode(TIMER_A1_BASE, &upMetronome);
-        Interrupt_enableInterrupt(INT_TA1_N);
-        Interrupt_enableMaster();
-
-    // Initialize Hann Window
- int n;
- for(n = 0; n < SAMPLE_LENGTH; n++)
- {
-     hann[n] = 0.5f - 0.5f * cosf((2 * PI * n) / (SAMPLE_LENGTH - 1));
- }
- printf("prova\n");
-    // /* Configuring Timer_A to have a period of approximately 500ms and
-    //  * an initial duty cycle of 10% of that (3200 ticks)  */
-   Timer_A_generatePWM(TIMER_A2_BASE, &pwmConfig);
-// 
-    // /* Initializing ADC (MCLK/1/1) */
- ADC14_enableModule();
- ADC14_initModule(ADC_CLOCKSOURCE_MCLK, ADC_PREDIVIDER_1, ADC_DIVIDER_1, 0);
-// 
- ADC14_setSampleHoldTrigger(ADC_TRIGGER_SOURCE5, false);
-// 
-//   /* Configuring GPIOs (4.3 A10) */
- GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P4, GPIO_PIN3,
-                                            GPIO_TERTIARY_MODULE_FUNCTION);
- printf("prova3.1\n");
-//   /* Configuring ADC Memory */
- ADC14_configureSingleSampleMode(ADC_MEM2, true);
- ADC14_configureConversionMemory(ADC_MEM2, ADC_VREFPOS_AVCC_VREFNEG_VSS,
-                                 ADC_INPUT_A10, false);
-// 
-//   /* Set ADC result format to signed binary */
- ADC14_setResultFormat(ADC_SIGNED_BINARY);
- printf("prova3.2\n");
- /* Configuring DMA module */
- DMA_enableModule();
- DMA_setControlBase(MSP_EXP432P401RLP_DMAControlTable);
-// 
- DMA_disableChannelAttribute(DMA_CH7_ADC14,
-                             UDMA_ATTR_ALTSELECT | UDMA_ATTR_USEBURST |
-                             UDMA_ATTR_HIGH_PRIORITY |
-                             UDMA_ATTR_REQMASK);
-// 
- /* Setting Control Indexes. In this case we will set the source of the
-  * DMA transfer to ADC14 Memory 0
-  *  and the destination to the
-  * destination data array. */
- DMA_setChannelControl(
-     UDMA_PRI_SELECT | DMA_CH7_ADC14,
-     UDMA_SIZE_16 | UDMA_SRC_INC_NONE |
-     UDMA_DST_INC_16 | UDMA_ARB_1);
- DMA_setChannelTransfer(UDMA_PRI_SELECT | DMA_CH7_ADC14,
-                            UDMA_MODE_PINGPONG, (void*) &ADC14->MEM[2],
-                            data_array1, SAMPLE_LENGTH);
- printf("prova3.3\n");
- DMA_setChannelControl(
-     UDMA_ALT_SELECT | DMA_CH7_ADC14,
-     UDMA_SIZE_16 | UDMA_SRC_INC_NONE |
-     UDMA_DST_INC_16 | UDMA_ARB_1);
- DMA_setChannelTransfer(UDMA_ALT_SELECT | DMA_CH7_ADC14,
-                            UDMA_MODE_PINGPONG, (void*) &ADC14->MEM[2],
-                            data_array2, SAMPLE_LENGTH);
- printf("prova3.4\n");
- /* Assigning/Enabling Interrupts */
- DMA_assignInterrupt(DMA_INT1, 7);
- printf("prova3.4.1\n");
- Interrupt_enableInterrupt(INT_DMA_INT1);
- printf("prova3.4.2\n");
- DMA_assignChannel(DMA_CH7_ADC14);
- printf("prova3.4.3\n");
- DMA_clearInterruptFlag(7);
- printf("prova3.4.4\n");
- Interrupt_enableMaster();
- printf("prova3.5\n");
- /* Now that the DMA is primed and setup, enabling the channels. The ADC14
-  * hardware should take over and transfer/receive all bytes */
- DMA_enableChannel(7);
- ADC14_enableConversion();
-
-    printf("prova4\n");
-    int32_t MCLK = CS_getMCLK();
-    int32_t ACLK = CS_getACLK();
-    int32_t SMCLK = CS_getSMCLK();
-    int32_t BCLK = CS_getBCLK();
-    int32_t DCO = CS_getDCOFrequency();
-    int32_t HSM = CS_getHSMCLK();
-
-    printf("MCLK\t%d\nACLK\t%d\nSMCLK\t%d\nBLCK\t%d\nDCO\t%d\nHSM\t%d\n",
-    MCLK, ACLK, SMCLK, BCLK, DCO, HSM);
     selectedOptionFlag = 0;
 
-    printf("%d\n", selectedOptionFlag);
     printChoice();
     selectOption();
+
     while(1)
     { 
     if(OldFlag==selectedOptionFlag)
@@ -375,53 +348,30 @@ int main(void)
                 selectOption();
                     break;
             case 72:
-                drawBar(&selectedOptionFlag);
+                drawBar();
                 printChoice();
                 selectOption();
                     break;
             case 73:
-                metronomeBPMValue(&selectedOptionFlag);
+                metronomeBPMValue();
                 printChoice();
                 selectOption();
                     break;
         }
         OldFlag=selectedOptionFlag;
-        printf("%d\n", selectedOptionFlag);
     }
  }
 }
 
-
-
-
-/* Completion interrupt for ADC14 MEM0 */
-void DMA_INT1_IRQHandler(void)
-{
-    // /* Switch between primary and alternate bufferes with DMA's PingPong mode */
- if(DMA_getChannelAttribute(7) & UDMA_ATTR_ALTSELECT)
+void hannConfig(){
+    // Initialize Hann Window
+ uint8_t n;
+ for(n = 0; n < SAMPLE_LENGTH; ++n)
  {
-     DMA_setChannelControl(
-         UDMA_PRI_SELECT | DMA_CH7_ADC14,
-         UDMA_SIZE_16 | UDMA_SRC_INC_NONE |
-         UDMA_DST_INC_16 | UDMA_ARB_1);
-     DMA_setChannelTransfer(UDMA_PRI_SELECT | DMA_CH7_ADC14,
-                            UDMA_MODE_PINGPONG, (void*) &ADC14->MEM[2],
-                            data_array1, SAMPLE_LENGTH);
-     switch_data = 1;
+     hann[n] = 0.5f - 0.5f * cosf((2 * PI * n) / (SAMPLE_LENGTH - 1));
  }
- else
- {
-     DMA_setChannelControl(
-         UDMA_ALT_SELECT | DMA_CH7_ADC14,
-         UDMA_SIZE_16 | UDMA_SRC_INC_NONE |
-         UDMA_DST_INC_16 | UDMA_ARB_1);
-     DMA_setChannelTransfer(UDMA_ALT_SELECT | DMA_CH7_ADC14,
-                            UDMA_MODE_PINGPONG, (void*) &ADC14->MEM[2],
-                            data_array2, SAMPLE_LENGTH);
-     switch_data = 0;
- }
+
 }
-
 
 void cleanScreen(){
     Graphics_clearDisplay(&g_sContext);
@@ -481,25 +431,24 @@ void selectOption(){
 }
 
 void incrementBPMvalue(){
-    if(selectedOptionFlag & BIT3 && printedBPM < 600 ){
+    if(selectedOptionFlag & BIT3 && printedBPM < 500 ){
             printedBPM += 100;
-    }else if(selectedOptionFlag & BIT4 && printedBPM < 650 ){
+    }else if(selectedOptionFlag & BIT4 && printedBPM < 550 ){
             printedBPM += 10;
-    }else if(selectedOptionFlag & BIT5 && printedBPM < 655 ){
+    }else if(selectedOptionFlag & BIT5 && printedBPM < 555 ){
             printedBPM += 1;
    }
   //  printf("\t%d\n",printedBPM);
 }
 
 void decrementBPMvalue(){
-    if(selectedOptionFlag & BIT3 && printedBPM > 150 ){
+    if(selectedOptionFlag & BIT3 && printedBPM > 140 ){
             printedBPM -= 100;
     }else if(selectedOptionFlag & BIT4 && printedBPM > 40 ){
             printedBPM -= 10;
-    }else if(selectedOptionFlag & BIT5 && printedBPM > 30 ){
+    }else if(selectedOptionFlag & BIT5 && printedBPM > 31 ){
             printedBPM -= 1;
     }
-    //printf("\t%d\n",printedBPM);
 }
 
 char* printChar(){
@@ -528,20 +477,15 @@ void metronomeBPMValue(){
     }else{
         if(selectedOptionFlag & BIT7){
             selectedDigit();
-            //OldFlag=selectedOptionFlag;
-            //setup timerx and start counter
-            upMetronome.timerPeriod = BPM_COUNTER(printedBPM);
-           // printf("BPM_val:%d\n ",upMetronome.timerPeriod);
-            Timer_A_configureUpMode(TIMER_A1_BASE, &upMetronome);
-        //    Interrupt_enableInterrupt(INT_TA1_N);
+            upMetronome.timerPeriod = BPM_COUNTER(printedBPM);  /*  Timer*/
+            Timer_A_configureUpMode(TIMER_A1_BASE, &upMetronome);/* Setup*/
 
-            Timer_A_startCounter(TIMER_A1_BASE, TIMER_A_UP_MODE);//metronome timer
+            Timer_A_startCounter(TIMER_A1_BASE, TIMER_A_UP_MODE);//metronome timer start
            while(selectedOptionFlag & BIT7){
                 PCM_gotoLPM0();
             }
-            Timer_A_stopTimer(TIMER_A1_BASE);//metronome timer
+            Timer_A_stopTimer(TIMER_A1_BASE);//metronome timer stop
             Timer_A_clearTimer(TIMER_A1_BASE);
-            //prova = 0;
             selectedDigit();
             OldFlag=selectedOptionFlag;
             oldBPM=printedBPM;
@@ -552,8 +496,6 @@ void metronomeBPMValue(){
             Graphics_drawStringCentered(&g_sContext,(int8_t *)BPM,AUTO_STRING_LENGTH,64,64,OPAQUE_TEXT);
             GrContextFontSet(&g_sContext, &g_sFontFixed6x8);
             selectedDigit();
-       // printf("BPM_val:%d\t%d\n ",BPM_val, &BPM_val);
-       // printf("BPM_val: %d\t%d\t%d\n ",ptr, &ptr);
             OldFlag=selectedOptionFlag;
             oldBPM=printedBPM;
       }
@@ -612,11 +554,11 @@ void selectedDigit(){
 }
 
 
-void drawBar(uint8_t * Flag){
+void drawBar(){
     cleanScreen();
     int i;
 
-    while(*Flag !=0){
+    while(selectedOptionFlag !=0){
 
 
         char note = noteDetection();
@@ -868,17 +810,40 @@ int noteCorrection(char note){
 }
 
 void buzz(){
-  //  ++prova;
-  //  printf("\tbuzz %d \n",prova);
-   // if(selectedOptionFlag & BIT1){
         compareConfigBuzzer.compareValue = ON;
         Timer_A_initCompare(TIMER_A0_BASE, &compareConfigBuzzer);
-//    }else{
         uint_fast8_t i;
         for(i=0; i < 22500; ++i);
         compareConfigBuzzer.compareValue = OFF;
         Timer_A_initCompare(TIMER_A0_BASE, &compareConfigBuzzer);
-//    }
+}
+
+/* Completion interrupt for ADC14 MEM0 */
+void DMA_INT1_IRQHandler(void)
+{
+    // /* Switch between primary and alternate bufferes with DMA's PingPong mode */
+ if(DMA_getChannelAttribute(7) & UDMA_ATTR_ALTSELECT)
+ {
+     DMA_setChannelControl(
+         UDMA_PRI_SELECT | DMA_CH7_ADC14,
+         UDMA_SIZE_16 | UDMA_SRC_INC_NONE |
+         UDMA_DST_INC_16 | UDMA_ARB_1);
+     DMA_setChannelTransfer(UDMA_PRI_SELECT | DMA_CH7_ADC14,
+                            UDMA_MODE_PINGPONG, (void*) &ADC14->MEM[2],
+                            data_array1, SAMPLE_LENGTH);
+     switch_data = 1;
+ }
+ else
+ {
+     DMA_setChannelControl(
+         UDMA_ALT_SELECT | DMA_CH7_ADC14,
+         UDMA_SIZE_16 | UDMA_SRC_INC_NONE |
+         UDMA_DST_INC_16 | UDMA_ARB_1);
+     DMA_setChannelTransfer(UDMA_ALT_SELECT | DMA_CH7_ADC14,
+                            UDMA_MODE_PINGPONG, (void*) &ADC14->MEM[2],
+                            data_array2, SAMPLE_LENGTH);
+     switch_data = 0;
+ }
 }
 
 void ADC14_IRQHandler(void) {
@@ -891,6 +856,7 @@ void ADC14_IRQHandler(void) {
     resultBuffer[0] = ADC14_getResult(ADC_MEM0); // left/right
     resultBuffer[1] = ADC14_getResult(ADC_MEM1); // up/down
 
+      /*up down */
     if (resultBuffer[1] > 15000) {
       if (selectedOptionFlag < 2) {
         selectedOptionFlag ^= BIT0;
@@ -904,7 +870,7 @@ void ADC14_IRQHandler(void) {
           decrementBPMvalue();
 
       }
-      /*left right ok*/
+      /*left right */
     } else if (resultBuffer[0] > 15000 && selectedOptionFlag > 3) {
       if (selectedOptionFlag & BIT3) {
         selectedOptionFlag ^= BIT3;
@@ -967,11 +933,7 @@ void PORT5_IRQHandler(void) {
 void TA1_N_IRQHandler(void){ //metronome control
 
     Timer_A_clearInterruptFlag(TIMER_A1_BASE);
-   // selectedOptionFlag ^= BIT1;
-    //TIMER_A1->CCTL[0] &= ~TIMER_A_CCTLN_CCIFG;
     buzz();
-   //  printf("ciao\n");
-    //Timer_A_stopTimer(TIMER_A1_BASE);
 }
 
 /* selectedOption Flag
